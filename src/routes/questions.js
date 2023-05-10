72de8fb5588048baf0e43b9b92e6641fa1ac9d01
Thomabed Thomabed <thomabed@yahoo.com>
2023-05-10 23:02:23 +0000

    Added more questions to my quiz

--- src/routes/questions.js
+++ src/routes/questions.js
@@ -8,33 +8,167 @@
  */
 export const questions = [
     {
-        question: "This is the first question.",
+        question: "Which one is used for cutting Text",
         answers: [
-            "True",
-            "False"
+            "Scissors",
+            "Ctrl+Shift+V",
+            "Ctrl+V",
+            "Ctrl+X"
         ],
-        correct: 0,
+        correct: 3,
         time: 20
     },
     {
-        question: "Who made this question?",
+        question: "How many People have Alzheimer's?",
         answers: [
-            "Ben Thomas",
-            "Mr. Bonner",
-            "Sam Mote",
-            "Camryn Verhey"
+            "17,309,300",
+            "6,712,478",
+            "1,234,567",
+            "I forget how many"
         ],
         correct: 1,
         time: 20
     },
     {
-        question: "How much time is left in this class?",
+        question: "Which one is the Colon?",
         answers: [
-            "So much",
-            "A respectable amount",
-            "Not enough"
+            ";",
+            "*",
+            ":",
+        ],
+        correct: 2,
+        time: 20
+    },
+    {
+        question: "How many toes does an Elephant have?",
+        answers: [
+            "5",
+            "4",
+            "3",
+            "6"
+        ],
+        correct: 0,
+        time: 20
+    },
+    {
+        question: "Which one is used for Pasting Text?",
+        answers: [
+            "Ctrl+C",
+            "Ctrl+Alt+V",
+            "Ctrl+V",
+            "Ctrl+X"
+        ],
+        correct: 2,
+        time: 20
+    },
+    {
+        question: "Which Hotkey is used for copying?",
+        answers: [
+        "Ctrl+C",
+        "Ctrl+Shift+V",
+        "Ctrl+V",
+        "Ctrl+X"
+        ],
+        correct: 0,
+        time: 20
+    },
+    {
+        question: "which hotkey is used for a Hyperlink?",
+        answers: [
+        "Ctrl+K",
+        "Ctrl+Alt+L",
+        "Ctrl+L",
+        "Ctrl+W"
+        ],
+        correct: 0,
+        time: 20
+    },
+    {
+        question: "Which Hotkey is used for Closing a tab?",
+        answers: [
+        "Ctrl+K",
+        "Ctrl+FN+L",
+        "Ctrl+L",
+        "Ctrl+W"
+        ],
+        correct: 3,
+        time: 20
+    },
+    {
+        question: "How do you make a left indent?",
+        answers: [
+        "Ctrl+K",
+        "Ctrl+Alt+L",
+        "Ctrl+T",
+        "Ctrl+W"
+        ],
+        correct: 2,
+        time: 20
+    },
+    {
+        question: "How do you save something?",
+        answers: [
+        "Ctrl+J",
+        "Ctrl+Alt+I",
+        "Ctrl+E",
+        "Ctrl+S"
+        ],
+        correct: 3,
+        time: 20
+    },
+    {
+        question: "How do you Center a document?",
+        answers: [
+        "Ctrl+J",
+        "Ctrl+CMD+I",
+        "Ctrl+E",
+        "Ctrl+S"
+        ],
+        correct: 2,
+        time: 20
+    },
+    {
+        question: "How do you Justify a document",
+        answers: [
+        "Ctrl+J",
+        "Ctrl+Alt+I",
+        "Ctrl+E",
+        "Ctrl+S"
+        ],
+        correct: 0,
+        time: 20
+    },
+    {
+        question: "How do you make a hanging indent?",
+        answers: [
+        "Ctrl+M",
+        "Ctrl+D",
+        "Ctrl+U",
+        "Ctrl+Q"
+        ],
+        correct: 3,
+        time: 20
+    },
+    {
+        question: "What is the hotkey for adding space after a paragraph?",
+        answers: [
+        "Ctrl+K",
+        "Ctrl+FN+L",
+        "Ctrl+T",
+        "Ctrl+W"
+        ],
+        correct: 0,
+        time: 20
+    },
+    {
+        question: "What's your favorite color of the Alphabet?",
+        answers: [
+        "True",
+        "False",
+        "All of the above"
         ],
         correct: 2,
         time: 20
     }
 ]
+ 
